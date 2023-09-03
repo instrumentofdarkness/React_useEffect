0b73d8b57e3c834c65e530605e3668fe72ec39d5
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-09-04 01:03:24 +0200

    fetch data, useEffect

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React hook - useEffect
 
-## How to run project
+## How to run the project
 
 1. Step 1: yarn
 2. Step 2: yarn start
--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const url = "https://fakestoreapi.com/products";
+
+// fetch data from the url
+
+// 2nd way: use axios library
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState([]);
+// for React: the state changes from empty array to data after fetching data from API
+
+
+  // 1st way: use fetch
+useEffect(() => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => setData(data))
+    .catch((error) => console.log(error));
+}, [productId]);
+
+getData();
+// useEffect: run the function getData() only once
+// if we don't use useEffect, the function getData() will run infinitely
+// because every time the state changes, the function getData() will run again
+// and the state changes again, and the function getData() will run again and again
+// []: dependency
+
+  return (
+    <div className="App">
+      <h1>React hook: useEffect</h1>
+      <h2>Fetch data from API</h2>
+      {data.map((item) => item.title)}
+    </div>
+  );
 }
 
 export default App;
