b58d048dae097b11835ace12394573e403efb188
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-09-05 01:09:15 +0200

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 const url = "https://fakestoreapi.com/products";
 
 // fetch data from the url
@@ -9,31 +9,64 @@ const url = "https://fakestoreapi.com/products";
 
 function App() {
   const [data, setData] = useState([]);
-// for React: the state changes from empty array to data after fetching data from API
-
-
+  // for React: the state changes from empty array to data after fetching data from API
+  const [isLoading, setIsLoading] = useState(true);
   // 1st way: use fetch
-useEffect(() => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => setData(data))
-    .catch((error) => console.log(error));
-}, [productId]);
 
-getData();
-// useEffect: run the function getData() only once
-// if we don't use useEffect, the function getData() will run infinitely
-// because every time the state changes, the function getData() will run again
-// and the state changes again, and the function getData() will run again and again
-// []: dependency
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // change the isLoading state to false
+  //       setIsLoading(false);
+  //       setData(data);
+  //     })
 
-  return (
-    <div className="App">
-      <h1>React hook: useEffect</h1>
-      <h2>Fetch data from API</h2>
-      {data.map((item) => item.title)}
-    </div>
-  );
+  //     .catch((error) => console.log(error));
+  // }, []);
+
+  // using async/await
+  async function getData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    setIsLoading(false);
+    setData(data);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // 2nd way: use axios library
+  // function getData() {
+  //   axios
+  //     .get(url)
+  //     .then((response) => response.data)
+  //     .then((data) => {
+  //       setIsLoading(false);
+  //       setData(response.data);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }
+
+  // message on the screen: the data is loading
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    // useEffect: run the function getData() only once
+    // if we don't use useEffect, the function getData() will run infinitely
+    // because every time the state changes, the function getData() will run again
+    // and the state changes again, and the function getData() will run again and again
+    // []: dependency
+
+    return (
+      <div className="App">
+        <h1>React hook: useEffect</h1>
+        <h2>Fetch data from API</h2>
+        {data.map((item) => item.title)}
+      </div>
+    );
+  }
 }
 
 export default App;
