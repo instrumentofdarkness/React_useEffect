f74ad77051054d580e180d16e5f6c360f0d7eee4
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-10-04 10:15:39 +0200

    country search works

--- src/components/search/CountrySearch.jsx
+++ src/components/search/CountrySearch.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function SearchFormCountry() {
-    // function handleSubmit(event) {
-    //   onSearch(event.target.value);
-    // }
-    return (
-      <div>
-        <form>
-          <label>
-            Please enter the country name: < span className="search"> </span>
-            <input
-              type="text"
-              placeholder="Search"
-              name="search"
-              // onChange={handleSubmit}
-            />
-            <span className="search"> </span>
-            <button type="submit">Search</button>
-          </label>
-        </form>
-      </div>
-    );
+export default function SearchFormCountry({ setSearchTerm }) {
+  function handleSubmit(event) {
+    setSearchTerm(event.target.value);
   }
+  return (
+    <div>
+      <form>
+        <label>
+          Please enter the country name: <span className="search"> </span>
+          <input
+            type="text"
+            placeholder="Search"
+            name="search"
+            onChange={handleSubmit}
+          />
+          <span className="search"> </span>
+          <button type="submit">Search</button>
+        </label>
+      </form>
+    </div>
+  );
+}
